16448d0f99a7cab8da05732f9a44b4f59c4b95c4
Minxly <55247596+Minxly@users.noreply.github.com>
2019-12-04 18:53:13 -0500

    Update B.D.E.D.py

--- B.D.E.D.py
+++ B.D.E.D.py
@@ -1,4 +1,4 @@
-ffrom tkinter import *
+from tkinter import *
 from openpyxl import load_workbook
 from PIL import ImageTk, Image
 from tkinter import messagebox
@@ -16,23 +16,33 @@ def help_window(event):
     root_2.geometry("600x380")
 
     root_2.resizable(False, False)
-
+    
+    '#Creates the top menu frame on root_2'
     menuFrame_2 = Frame(root_2, bg="#060273", height="30")
     menuFrame_2.pack(fill=X)
 
+    '#Makes space on root_2'
     spaceFrame_4 = Frame(root_2, height="15")
     spaceFrame_4.pack(fill=X)
-
+    
+    '#Creates title frame on root_2' 
     tFrame_2 = Frame(root_2)
+    tFrame_2.pack()
+
+    '#Creates title frame on root_2'
     title_2 = Label(tFrame_2, text="Help")
     title_2.config(font=("Arial", 18))
     title_2.pack()
-    tFrame_2.pack()
-
+    
+    '#Makes space on roo_2'
     spaceFrame_5 = Frame(root_2, height="10")
     spaceFrame_5.pack(fill=X)
 
+    '#Creates the main frame to put label widgets on for root_2'
     mainFrame_2 = Frame(root_2, borderwidth="2", relief=SUNKEN, width="400", height="180")
+    mainFrame_2.pack()
+
+    '#Create and add label widgets to root_2'
     label_7 = Label(mainFrame_2, text="When Entering Yor Name Use Your Full Name")
     label_8 = Label(mainFrame_2, text="Eg: John Doe", fg="#060273")
     label_9 = Label(mainFrame_2, text="When Entering The Date Enter It In The Following Format: Month, Day, Year")
@@ -49,17 +59,21 @@ def help_window(event):
     label_12.grid(row=5)
     label_13.grid(row=6)
     label_14.grid(row=7)
-    mainFrame_2.pack()
-
+    
+    '#Makes space on root_2'
     spaceFrame_6 = Frame(root_2, height="10")
     spaceFrame_6.pack(fill=X)
 
+    '#Creates the button frame on root_2'
     bFrame_2 = Frame(root_2, height="20")
+    bFrame_2.pack()
+    
+    '#Creates enter button that is closes the window'
     enter_2 = Button(bFrame_2, text="Close Window", command=close_window, width=12, height=2)
     enter_2.config(font=("Arial", 18))
     enter_2.grid(row=0, columnspan=4)
-    bFrame_2.pack()
-
+    
+    '#Creates bottom menu frame on root_2'
     bMenuFrame_2 = Frame(root_2, bg="#060273", height="30")
     bMenuFrame_2.pack(fill=X, side=BOTTOM)
 
@@ -71,38 +85,46 @@ def end(event):
 
 
 def print_check():
-    eName = entry_1.get()
-    eDate = entry_2.get()
-    eItem = entry_3.get()
-    eCost = entry_4.get()
-    eProject = entry_5.get()
-    eSupplier = entry_6.get()
+    eN = entry_1.get()
+    eD = entry_2.get()
+    eI = entry_3.get()
+    eC = entry_4.get()
+    eP = entry_5.get()
+    eS = entry_6.get()
 
-    workbook_name = "Budgeting.xlsx"
+    '#Opens B.D.E.D.xlsx'
+    workbook_name = "B.D.E.D.xlsx"
     wb = load_workbook(workbook_name)
+    
+    '#Sets page variable to allow editing of B.D.E.D.xlsx'
     page = wb.active
+    
+    '#Creates list of all the entry box data'
+    all_entries = [[eN, eD, eI, eC, eP, eS]]
 
-    all_entries = [[eName, eDate, eItem, eCost, eProject, eSupplier]]
-
-    if eCost.isalpha():
+    '#Error checking for each entries length and for cost if it is a number or not'
+    if eC.isalpha():
         messagebox.showerror("Error", "Please Enter The Cost Properly", icon="warning")
-    if len(eName) == 0:
+    if len(eN) == 0:
         messagebox.showerror("Error", "Please Enter Your Name", icon="warning")
-    if len(eDate) == 0:
+    if len(eD) == 0:
         messagebox.showerror("Error", "Please Enter The Date", icon="warning")
-    if len(eItem) == 0:
+    if len(eI) == 0:
         messagebox.showerror("Error", "Please Enter The Item Purchased", icon="warning")
-    if len(eCost) == 0:
+    if len(eC) == 0:
         messagebox.showerror("Error", "Please Enter The Cost", icon="warning")
-    if len(eProject) == 0:
+    if len(eP) == 0:
         messagebox.showerror("Error", "Please Enter Your Current Project", icon="warning")
-    if len(eSupplier) == 0:
+    if len(eS) == 0:
         messagebox.showerror("Error", "Please Enter The Supplier", icon="warning")
-    if len(eName) and len(eDate) and len(eItem) and len(eCost) and len(eProject) and len(eSupplier) >= 1 and eCost.isdigit():
+        
+    '#Checks if all the data was entered properly then adds it to B.D.E.D.xlsx'
+    if len(eN) and len(eD) and len(eI) and len(eC) and len(eP) and len(eS) >= 1 and eC.isdigit():
         ayn = messagebox.askquestion("B.D.E.D", "Are You Sure")
         if ayn == 'yes':
             for info in all_entries:
                 page.append(info)
+            '#Saves the file then closes the window to prevent duplicate entries'
             wb.save(filename=workbook_name)
             root.destroy()
 
@@ -115,27 +137,33 @@ root.geometry("600x380")
 
 root.resizable(False, False)
 
-'#top menu frame'
+'#Creates the top menu frame'
 menuFrame_1 = Frame(root, bg="#060273", height="30")
 menuFrame_1.pack(fill=X)
 
-'#make space'
+'#Makes space on GUI'
 spaceFrame_1 = Frame(root, height="5")
 spaceFrame_1.pack(fill=X)
 
-'#title frame'
+'#Creates title frame'
 tFrame_1 = Frame(root)
+tFrame_1.pack()
+
+'#Creates and adds a title to the GUI'
 title_1 = Label(tFrame_1, text="Enter Information")
 title_1.config(font=("Arial", 18))
 title_1.pack()
-tFrame_1.pack()
 
-'#main frame'
+
+'#Creates the main frame to put label and entry box frame on'
 mainFrame_1 = Frame(root, borderwidth="2", relief=SUNKEN, width="400", height="180")
 mainFrame_1.pack(fill=None, expand=False)
 
-'#label frame'
+'#Creates label frame'
 lFrame_1 = Frame(mainFrame_1)
+lFrame_1.pack(side=LEFT)
+
+'#Create and add label widgets to GUI'
 label_1 = Label(lFrame_1, text="Name", bg="white", fg="black", height=2)
 label_2 = Label(lFrame_1, text="Date of Order", bg="white", fg="black", height=2)
 label_3 = Label(lFrame_1, text="Item Purchased", bg="white", fg="black", height=2)
@@ -148,10 +176,12 @@ label_3.grid(row=5, sticky=W)
 label_4.grid(row=6, sticky=W)
 label_5.grid(row=7, sticky=W)
 label_6.grid(row=9, sticky=W)
-lFrame_1.pack(side=LEFT)
 
-'#entry box frame'
+'#Creates entry box frame'
 eFrame_1 = Frame(mainFrame_1)
+eFrame_1.pack(side=RIGHT)
+
+'#Create and add entry box widgets to GUI'
 entry_1 = Entry(eFrame_1)
 entry_2 = Entry(eFrame_1)
 entry_3 = Entry(eFrame_1)
@@ -164,25 +194,29 @@ entry_3.grid(row=3, ipady=5)
 entry_4.grid(row=4, ipady=5)
 entry_5.grid(row=5, ipady=5)
 entry_6.grid(row=6, ipady=5)
-eFrame_1.pack(side=RIGHT)
 
-'#make space'
+'#Makes space on GUI'
 spaceFrame_2 = Frame(root, height="5")
 spaceFrame_2.pack(fill=X)
 
-'#button frame'
+'#Creates the button frame'
 bFrame_1 = Frame(root)
+bFrame_1.pack()
+
+'#Creates enter button that is bound to print_check'
 enter_1 = Button(bFrame_1, text="Enter", command=print_check, width=12, height=2)
 enter_1.config(font=("Arial", 18))
 enter_1.grid(row=0, columnspan=4)
-bFrame_1.pack()
 
-'#make space'
+'#Makes space on GUI'
 spaceFrame_3 = Frame(root, height="5")
 spaceFrame_3.pack(fill=X)
 
-'#bottom menu frame'
+'#Creates bottom menu frame'
 bMenuFrame_1 = Frame(root, bg="#060273", height="30")
+bMenuFrame_1.pack(fill=X)
+
+'#Creates first canvas on bottom menu frame and adds power_3.png to it'
 canvas_1 = Canvas(bMenuFrame_1, height="30", width="570", bg="#060273", highlightthickness=0)
 powerButton = Image.open("power3.png")
 powerButton = powerButton.resize((20, 20), Image.ANTIALIAS)
@@ -190,6 +224,8 @@ myimg = ImageTk.PhotoImage(powerButton)
 canvas_1.create_image(15, 15, image=myimg)
 canvas_1.bind("<Button>", end)
 canvas_1.pack(side=LEFT)
+
+'#Creates second canvas on bottom menu frame and adds help_button.png to it'
 canvas_2 = Canvas(bMenuFrame_1, height="30", bg="#060273", highlightthickness=0)
 questionButton = Image.open("help_button.png")
 questionButton = questionButton.resize((20, 20), Image.ANTIALIAS)
@@ -197,6 +233,5 @@ myimg_2 = ImageTk.PhotoImage(questionButton)
 canvas_2.create_image(15, 15, image=myimg_2)
 canvas_2.bind("<Button>", help_window)
 canvas_2.pack(side=RIGHT)
-bMenuFrame_1.pack(fill=X)
 
 root.mainloop()
